ac22095fa7d4b13d45cffba651a50daf9ab71c1a
dookiehero <lethan96@gmail.com>
2018-08-15 13:00:13 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,12 +5,13 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/fbi$/;
+      botRegex = /^\/FBI remove Tom$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
     this.res.end();
+    remove(8715476);
   } else {
     console.log("don't care");
     this.res.writeHead(200);
