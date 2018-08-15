a2b1b32679a2dbc0c6d1fd1b238ab74f660bcc76
dookiehero <lethan96@gmail.com>
2018-08-15 14:12:43 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\"6073215"$/;
 
-  if(request.sender_id && botRegex.test(request.sender_id)) {
+  if(request.user_id && botRegex.test(request.user_id)) {
     this.res.writeHead(200);
     postMessage();
     this.res.end();
