abbff5134f45d956c01ca19380fb2daa5137dfbf
dookiehero <lethan96@gmail.com>
2018-08-15 14:37:04 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\hello$/;
 
-  if(request.name == "ethan") {
+  if(request.user_id == "6073141") {
     this.res.writeHead(200);
     postMessage();
     this.res.end();
