a51f9c0d6e3ff9ff24d164b9f379a2badcbcc911
dookiehero <lethan96@gmail.com>
2018-08-15 13:23:07 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool()
+  botResponse = "Shut up lol"
 
   options = {
     hostname: 'api.groupme.com',
